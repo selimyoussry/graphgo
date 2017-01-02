672a801a907d1f8604e5de544040950a18fe2b89
selim <selim@wearkinetic.com>
2017-01-02 12:57:46 -0500

    errors

--- errors.go
+++ errors.go
@@ -1,22 +1,50 @@
 package graphgo
 
-import (
-	"errors"
-	"fmt"
-)
-
-func errNodeNotFound(key string) error {
-	return errors.New(fmt.Sprintf("Could not find node %s", key))
+// Error implements our custom errors
+type Err struct {
+	code  string
+	Props map[string]interface{}
 }
 
-func errEdgeNotFound(key string) error {
-	return errors.New(fmt.Sprintf("Could not find edge %s", key))
+// Error to implement the error interface
+func (e *Err) Error() string {
+	return e.code
 }
 
-func errorNodePropNotFound(nodeKey, key string) error {
-	return errors.New(fmt.Sprintf("Could not find node (%s)'s' property %s", nodeKey, key))
+// NewErr instanciates
+func NewErr(code string, props map[string]interface{}) *Err {
+	return &Err{
+		code:  code,
+		Props: props,
+	}
 }
 
-func errorEdgePropNotFound(nodeKey, key string) error {
-	return errors.New(fmt.Sprintf("Could not find node (%s)'s' property %s", nodeKey, key))
+// errNodeNotFound when a node is not found in the graph, given its key
+func errNodeNotFound(key string) *Err {
+	return NewErr(ERR_NODE_NOT_FOUND, map[string]interface{}{
+		"key": key,
+	})
+}
+
+// errEdgeNotFound when an edge is not found in the graph, given its key
+func errEdgeNotFound(key string) *Err {
+	return NewErr(ERR_EDGE_NOT_FOUND, map[string]interface{}{
+		"key": key,
+	})
+}
+
+// errorNodePropNotFound when a property is not found on a node
+func errorNodePropNotFound(nodeKey, prop string) *Err {
+	return NewErr(ERR_NODE_PROP_NOT_FOUND, map[string]interface{}{
+		"nodeKey": nodeKey,
+		"prop":    prop,
+	})
+}
+
+// errorEdgePropNotFound when a property is not found on a node
+func errorEdgePropNotFound(edgeKey, prop string) *Err {
+	return NewErr(ERR_EDGE_PROP_NOT_FOUND, map[string]interface{}{
+		"edgeKey": edgeKey,
+		"prop":    prop,
+	})
 }
